00489a127660e932e55a679aeaf44a2b5a8a7aa1
Aldo Serrano <aldoantonio1723@gmail.com>
2026-01-09 21:33:21 -0600

    Feat: Agrega el paquete dto
    -Añade en el record SerieDTO

--- back-end/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ back-end/src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,32 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String mostrarMensaje(){
-        return "ESTE ES MI PRIMER MENSAJE EN MI APLICACION WEB";
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return repository.findAll().stream()
+                .map(serie -> new SerieDTO(
+                        serie.getTitulo(),
+                        serie.getTotalTemporadas(),
+                        serie.getEvaluacion(),
+                        serie.getPoster(),
+                        serie.getGenero(),
+                        serie.getActores(),
+                        serie.getSinopsis()
+                        ))
+                .collect(Collectors.toList());
     }
 }
